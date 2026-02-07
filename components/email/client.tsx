424523aecc6bacfd748f46fe40f161b00a4a79c8
keypad <githubs15@outlook.com>
2026-02-07 19:29:22 +0000

    feat: add mobile email detail view with list/detail toggle

--- components/email/client.tsx
+++ components/email/client.tsx
@@ -1,36 +1,52 @@
 "use client"
 
 import Compose from "@/components/email/compose"
-import { Provider } from "@/components/email/context"
+import { Provider, useEmail } from "@/components/email/context"
 import Detail from "@/components/email/detail"
 import List from "@/components/email/list"
 import Overlay from "@/components/email/overlay"
 import Sidebar from "@/components/email/sidebar"
 import Toolbar from "@/components/email/toolbar"
 
-export default function Client() {
+function Layout() {
+	const { selected } = useEmail()
+
 	return (
-		<Provider>
-			<div className="flex h-screen overflow-hidden bg-[#1e1c1a]">
-				<div className="hidden w-56 shrink-0 md:block">
-					<Sidebar />
-				</div>
-				<Overlay />
-				<div className="flex min-w-0 flex-1 flex-col">
-					<Toolbar />
-					<div className="flex min-h-0 flex-1">
-						<div className="w-full flex-shrink-0 border-r border-white/[0.06] md:w-80">
-							<div className="flex h-full flex-col">
-								<List />
-							</div>
+		<div className="flex h-screen overflow-hidden bg-[#1e1c1a]">
+			<div className="hidden w-56 shrink-0 md:block">
+				<Sidebar />
+			</div>
+			<Overlay />
+			<div className="flex min-w-0 flex-1 flex-col">
+				<Toolbar />
+				<div className="flex min-h-0 flex-1">
+					<div
+						className={`w-full flex-shrink-0 border-r border-white/[0.06] md:block md:w-80 ${
+							selected ? "hidden" : "block"
+						}`}
+					>
+						<div className="flex h-full flex-col">
+							<List />
 						</div>
-						<div className="hidden min-w-0 flex-1 md:flex">
-							<Detail />
-						</div>
+					</div>
+					<div
+						className={`min-w-0 flex-1 md:flex ${
+							selected ? "flex" : "hidden"
+						}`}
+					>
+						<Detail />
 					</div>
 				</div>
 			</div>
 			<Compose />
+		</div>
+	)
+}
+
+export default function Client() {
+	return (
+		<Provider>
+			<Layout />
 		</Provider>
 	)
 }
