7bae19f7837496dab36ed729f35f54a0e3c0310c
keypad <githubs15@outlook.com>
2026-02-07 18:30:53 +0000

    feat: expand uniswap tokens with logos and defi tokens

--- components/providers/uniswap.ts
+++ components/providers/uniswap.ts
@@ -2,6 +2,10 @@ import type { ProviderModule, SwapToken } from "./types"
 import { toSmallest } from "./types"
 
 const ROUTER = "0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD"
+const WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+const QUOTER = "0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6"
+
+const TW = "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets"
 
 const TOKENS: SwapToken[] = [
 	{
@@ -10,6 +14,7 @@ const TOKENS: SwapToken[] = [
 		decimals: 18,
 		name: "Ether",
 		chainId: 1,
+		logo: `${TW}/${WETH}/logo.png`,
 	},
 	{
 		address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
@@ -17,6 +22,7 @@ const TOKENS: SwapToken[] = [
 		decimals: 6,
 		name: "USD Coin",
 		chainId: 1,
+		logo: `${TW}/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png`,
 	},
 	{
 		address: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
@@ -24,6 +30,7 @@ const TOKENS: SwapToken[] = [
 		decimals: 6,
 		name: "Tether",
 		chainId: 1,
+		logo: `${TW}/0xdAC17F958D2ee523a2206206994597C13D831ec7/logo.png`,
 	},
 	{
 		address: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
@@ -31,6 +38,7 @@ const TOKENS: SwapToken[] = [
 		decimals: 18,
 		name: "Dai",
 		chainId: 1,
+		logo: `${TW}/0x6B175474E89094C44Da98b954EedeAC495271d0F/logo.png`,
 	},
 	{
 		address: "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599",
@@ -38,6 +46,15 @@ const TOKENS: SwapToken[] = [
 		decimals: 8,
 		name: "Wrapped BTC",
 		chainId: 1,
+		logo: `${TW}/0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599/logo.png`,
+	},
+	{
+		address: WETH,
+		symbol: "WETH",
+		decimals: 18,
+		name: "Wrapped Ether",
+		chainId: 1,
+		logo: `${TW}/${WETH}/logo.png`,
 	},
 	{
 		address: "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984",
@@ -45,11 +62,81 @@ const TOKENS: SwapToken[] = [
 		decimals: 18,
 		name: "Uniswap",
 		chainId: 1,
+		logo: `${TW}/0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984/logo.png`,
+	},
+	{
+		address: "0x514910771AF9Ca656af840dff83E8264EcF986CA",
+		symbol: "LINK",
+		decimals: 18,
+		name: "Chainlink",
+		chainId: 1,
+		logo: `${TW}/0x514910771AF9Ca656af840dff83E8264EcF986CA/logo.png`,
+	},
+	{
+		address: "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9",
+		symbol: "AAVE",
+		decimals: 18,
+		name: "Aave",
+		chainId: 1,
+		logo: `${TW}/0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9/logo.png`,
+	},
+	{
+		address: "0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2",
+		symbol: "MKR",
+		decimals: 18,
+		name: "Maker",
+		chainId: 1,
+		logo: `${TW}/0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2/logo.png`,
+	},
+	{
+		address: "0x5A98FcBEA516Cf06857215779Fd812CA3beF1B32",
+		symbol: "LDO",
+		decimals: 18,
+		name: "Lido DAO",
+		chainId: 1,
+		logo: `${TW}/0x5A98FcBEA516Cf06857215779Fd812CA3beF1B32/logo.png`,
+	},
+	{
+		address: "0x6982508145454Ce325dDbE47a25d4ec3d2311933",
+		symbol: "PEPE",
+		decimals: 18,
+		name: "Pepe",
+		chainId: 1,
+		logo: `${TW}/0x6982508145454Ce325dDbE47a25d4ec3d2311933/logo.png`,
+	},
+	{
+		address: "0x95aD61b0a150d79219dCF64E1E6Cc01f0B64C4cE",
+		symbol: "SHIB",
+		decimals: 18,
+		name: "Shiba Inu",
+		chainId: 1,
+		logo: `${TW}/0x95aD61b0a150d79219dCF64E1E6Cc01f0B64C4cE/logo.png`,
+	},
+	{
+		address: "0xD533a949740bb3306d119CC777fa900bA034cd52",
+		symbol: "CRV",
+		decimals: 18,
+		name: "Curve DAO",
+		chainId: 1,
+		logo: `${TW}/0xD533a949740bb3306d119CC777fa900bA034cd52/logo.png`,
+	},
+	{
+		address: "0xc00e94Cb662C3520282E6f5717214004A7f26888",
+		symbol: "COMP",
+		decimals: 18,
+		name: "Compound",
+		chainId: 1,
+		logo: `${TW}/0xc00e94Cb662C3520282E6f5717214004A7f26888/logo.png`,
+	},
+	{
+		address: "0x4d224452801ACEd8B2F0aebE155379bb5D594381",
+		symbol: "APE",
+		decimals: 18,
+		name: "ApeCoin",
+		chainId: 1,
+		logo: `${TW}/0x4d224452801ACEd8B2F0aebE155379bb5D594381/logo.png`,
 	},
 ]
-
-const WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
-const QUOTER = "0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6"
 
 function tokenAddress(token: SwapToken): string {
 	if (token.symbol === "ETH") return WETH
